b08cbf276ee8a29b4e250cb79da479595fe82ee7
unknown <rodrigo.bs.548@gmail.com>
2022-03-26 11:03:47 -0300

    Mod. function file

--- functions.php
+++ functions.php
@@ -1,9 +1,15 @@
 <?php
 
-function teste() {
-    echo 'Mensagem Teste';
+function buymarket_add_woocommerce_support() {
+    add_theme_support('woocomerce');
 }
 
-add_action('dentro_da_home', 'teste');
+add_action('after_setup_theme', 'buymarket_add_woocommerce_support');
+
+function buymarkey_css() {
+    wp_register_style('style.css', get_template_directory_uri() . '/style.css', [], '1.0.0', false);
+}
+
+add_action('wp_enqueue_scripts', 'buymarket_css');
 
 ?>
